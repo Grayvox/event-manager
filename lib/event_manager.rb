9cf7e55ef44855cfe3557e610b68bab533bf185b
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-24 18:37:24 -0400

    Add zipcode checker

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,16 @@
 
 require 'csv'
 
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  end
+end
+
 ATTENDEES = 'event_attendees.csv'
 if File.exist?(ATTENDEES)
   contents = CSV.open(
@@ -14,5 +24,7 @@ end
 puts 'Event Manager Initialized!'
 contents.each do |row|
   name = row[:first_name]
-  puts name
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} - #{zipcode}"
 end
