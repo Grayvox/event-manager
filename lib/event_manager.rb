911b3865e9296a86714fe05b89a42c6577e5ba82
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-04-24 20:32:25 -0400

    Generate HTML from ERB templating
    Generates a new directory of each form letter in
    the form of an individual HTML file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -40,13 +40,17 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-  zip = row[:zipcode]
 
-  zipcode = clean_zipcode(zip)
-
-  legislators = legislators_by_zipcode(zip)
-
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
-  puts form_letter
+
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
